3d5d040e42eeb8fae7dd943c521291231647f4f1
gh0stwizard <vitaliy.tokarev@gmail.com>
2015-06-25 18:17:49 +0300

    added make.sh for staticperl

---
+++ make.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+APPNAME="tcp-proxy"
+STRIP="ppi" #ppi"
+LINKTYPE="static" #allow-dynamic"
+RC_FILE=${HOME}/.staticperlrc
+SP_FILE=${HOME}/staticperl
+BOOT_FILE="tcp-proxy.pl"
+
+
+if [ -f ${RC_FILE} ]; then
+        . ${RC_FILE}
+else
+        echo "${RC_FILE}: not found"
+        exit 1
+fi
+
+
+${SP_FILE} mkapp $APPNAME --boot ${BOOT_FILE} \
+-MGetopt::Long \
+-MIO::Socket \
+-MIO::Select \
+--strip ${STRIP} \
+--${LINKTYPE} \
+--usepacklists \
+$@
